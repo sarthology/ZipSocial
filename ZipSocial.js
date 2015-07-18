ea2fd45cd5b7cc12d7c91826f3b4c889d79bbcc5
Sarthak Sharma <7lovesharma7@gmail.com>
2015-07-19 02:22:40 +0530

    Page 2(posting comments):Ready

--- ZipSocial.js
+++ ZipSocial.js
@@ -1,4 +1,40 @@
+Comments = new Mongo.Collection("comments");
 if (Meteor.isClient) {
+  Template.body.helpers({
+    comments:function () {
+      return Comments.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-comment": function (event) {
+    event.preventDefault();
+
+    // Get value from form element
+    var comment = event.target.comment.value;
+
+    // Insert a comment
+    Comments.insert({
+      comment: comment,
+      email: "Yoda@starwars.com",
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    event.target.comment.value = "";
+    return false;
+  }
+  });
+
+  Template.registerHelper('formatDate', function(date) {
+  return moment(date).format('MM-DD-YYYY');
+  });
+
+  Template.discussion.events({
+   "click .delete": function () {
+     Comments.remove(this._id);
+   }
+ });
 
 }
 
