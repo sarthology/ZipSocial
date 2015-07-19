62453635d76e0b7ee1b4e1dd1ba574a442547195
Sarthak Sharma <7lovesharma7@gmail.com>
2015-07-19 15:58:28 +0530

    Fetching and storing information

--- client/index.js
+++ client/index.js
@@ -49,8 +49,34 @@
   }
   });
 
-  Template.registerHelper('formatDate', function(date) {
-  return moment(date).format('MM-DD-YYYY');
+  Template.registerHelper("usernameFromId", function (userId) {
+    var user = Meteor.users.findOne({_id: userId});
+    if (typeof user === "undefined") {
+        return "Anonymous";
+    }
+    if (typeof user.services.github !== "undefined") {
+        return user.services.github.username;
+    }
+    return user.username;
+  });
+
+  Template.registerHelper('imageFromId', function(userId) {
+    var user = Meteor.users.findOne({_id: userId});
+    if (typeof user === "undefined") {
+        return "Anonymous";
+    }
+    if (typeof user.services.github !== "undefined") {
+        return(user.profile.avatar_url);
+    }
+    //return user.username;
+  });
+
+  Template.registerHelper("timestampToTime", function (timestamp) {
+      var date = new Date(timestamp);
+      var hours = date.getHours();
+      var minutes = "0" + date.getMinutes();
+      var seconds = "0" + date.getSeconds();
+      return hours + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);
   });
 
   Template.discussion.events({
